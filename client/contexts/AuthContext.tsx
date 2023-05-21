e39a4999362cc473423baae8695918c539033033
Upal Barua <upalbarua52@gmail.com>
2023-05-21 09:22:59 +0600

    Added react-hook-form and tested firebase auth

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -1,9 +1,9 @@
 import { ReactNode, createContext, useContext } from 'react';
 import { app } from '@/firebase/firebase.config';
-import { getAuth } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 interface AuthContextProps {
-  printHelloWorld: () => void;
+  registerUser: (email: string, password: string) => void;
 }
 
 const AuthContext = createContext<AuthContextProps | null>(null);
@@ -11,12 +11,12 @@ const AuthContext = createContext<AuthContextProps | null>(null);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const auth = getAuth(app);
 
-  const printHelloWorld = () => {
-    console.log('Hello world');
+  const registerUser = (email: string, password: string) => {
+    return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const value = {
-    printHelloWorld,
+    registerUser,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,7 +1,28 @@
 import Navbar from '@/components/Navbar/Navbar';
+import { useForm } from 'react-hook-form';
+import { useAuth } from '@/contexts/AuthContext';
 
 const register = () => {
-  return <Navbar />;
+  // * Testing the functionality of firebase auth.
+
+  const { register, handleSubmit } = useForm();
+  const { registerUser } = useAuth();
+
+  const handleRegister = async ({ email, password }) => {
+    const res = await registerUser(email, password);
+    console.log(res);
+  };
+
+  return (
+    <section>
+      <Navbar />
+      <form onSubmit={handleSubmit(handleRegister)}>
+        <input type="text" {...register('email')} />
+        <input type="text" {...register('password')} />
+        <button type="submit">register</button>
+      </form>
+    </section>
+  );
 };
 
 export default register;
