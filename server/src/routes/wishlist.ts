36824142872314c8ca1560424f4dd15ba195dc11
Upal Barua <upalbarua52@gmail.com>
2023-05-31 00:59:00 +0600

    Did some changes in /wishlist routes

--- server/src/routes/wishlist.ts
+++ server/src/routes/wishlist.ts
@@ -21,46 +21,43 @@ router.get('/', async (req: Request, res: Response) => {
   }
 });
 
-// router.post('/', async (req: Request, res: Response) => {
-//   const wishlist = await Wishlist.create({
-//     userEmail: 'upal@mail.com',
-//     wishlist: ['646dcb8c7e9eeea0720850a6'],
-//   });
-
-//   res.json(wishlist);
-// });
-
 router.put('/', async (req: Request, res: Response) => {
   const { email, productId } = req.query;
+
+  if (!email || !productId) {
+    return res.status(400).json({ message: 'email or productId invalid' });
+  }
 
   try {
     const wishlist = await Wishlist.findOne({ userEmail: email }).populate(
       'products'
     );
 
-    if (!wishlist || Object.keys(wishlist).length === 0) {
-      const newWishlist = Wishlist.create({
+    if (!wishlist) {
+      const newWishlist = await Wishlist.create({
         userEmail: email,
         products: [productId],
       });
+
       return res.status(201).json(newWishlist);
     }
 
     const existingProduct = wishlist.products.find(
-      (product) => product?._id === productId
+      ({ _id }) => '' + _id === productId
     );
 
-    if (existingProduct) {
-      wishlist.products = wishlist.filter(
-        (product) => product?._id !== productId
+    if (existingProduct && Object.keys(existingProduct).length !== 0) {
+      wishlist.products = wishlist.products.filter(
+        ({ _id }) => '' + _id !== productId
       );
-      const updatedWishlist = wishlist.save();
-      return res.status(200).json(updatedWishlist);
+      const newWishlist = await wishlist.save();
+      return res.status(200).json(newWishlist);
     }
 
-    wishlist.products.push(productId);
-    const createdWishlist = await wishlist.save();
-    res.status(201).json(createdWishlist);
+    // wishlist.products.push(productId);
+    wishlist.products = [...wishlist.products, productId];
+    const newWishlist = await wishlist.save();
+    res.status(200).json(newWishlist);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
